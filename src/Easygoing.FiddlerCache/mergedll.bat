826d40dfc755ba80c254d8d3678c3cc62c0b3923
yinjun <yinjun@shootsoft.net>
2014-01-11 17:17:11 +0800

    add package bat

---
+++ src/Easygoing.FiddlerCache/mergedll.bat
@@ -0,0 +1,1 @@
+..\..\..\packages\ilmerge.2.13.0307\ilmerge /ndebug /target:library /targetplatform:v4 /out:Easygoing.FiddlerCache.dll Easygoing.FiddlerCache.dll /log Newtonsoft.Json.dll ObjectListView.dll
